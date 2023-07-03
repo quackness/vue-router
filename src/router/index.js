0d979c5f245b9ce6d71d4e6bc3a3bac41abae26b
Karolina Redden <dubajkaro@gmail.com>
2023-07-03 20:23:36 -0300

    add active link class

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: 'vue-school-active-link'
 });
 
 export default router;
