cd8d99b783281235c4e94e1cf83c1bbbd56e988c
Karolina Redden <dubajkaro@gmail.com>
2023-07-03 20:46:24 -0300

    add route parms and create a computed property

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -5,3 +5,13 @@
 </template>
 
 //in vue dev tools go to router view and inspect the params 
+
+<script setup>
+import { computed } from "vue";
+// computed
+const destinationId = computed(() => {
+  return parseInt(this.$route.params.id);
+});
+</script>
+
+//use parseInt to cast it into a number, all params coming from the url are strings
