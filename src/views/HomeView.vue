a21bc356f4961c18a12e4f5d4e70e2e33f941454
Karolina Redden <dubajkaro@gmail.com>
2023-07-03 18:59:48 -0300

    fetch data to the view from a json file and loop through the date - composistion api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>All destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </router-link>
+    </div>
   </div>
 </template>
+<script setup>
+import sourceData from "@/data.json";
+// import { defineComponent } from "@vue/composition-api";
+// import ref from "vue";
+//  setup() {
+//   return { destinations: sourceData.destinations}
+// }
+
+const destinations = sourceData.destinations;
+console.log(destinations);
+</script>
