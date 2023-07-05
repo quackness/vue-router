cdcd947a9c318f53a85a4336bc6ea0394cffc482
Karolina Redden <karolina.redden@dashhudson.com>
2023-07-05 13:44:40 -0300

    add logo and route it to home page

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav">
+      <router-link id="logo" to="/">Vue School Travel App</router-link>
       <router-link
         v-for="destination in destinations"
         :key="destination.id"
