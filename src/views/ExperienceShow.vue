7ec5511d5660f3f46b090b1d61b3ab45e1c6bce2
Karolina Redden <dubajkaro@gmail.com>
2023-07-09 11:26:13 -0300

    create computed properties for experiences

---
+++ src/views/ExperienceShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h1>{{ experience.name }}</h1>
+    <img :src="`/images/${experience.image}`" :alt="experience.name" />
+    <p>{{ experience.description }}</p>
+  </section>
+</template>
+<script setup>
+import sourceData from "@/data.json";
+
+const props = defineProps({
+  id: { type: Number, required: true },
+  experienceSlug: { type: String, required: true },
+});
+
+const destination = computed(() => {
+  return sourceData.destinations.find(
+    (destination) => destination.id === props.id
+  );
+});
+
+const experience = computed(() => {
+  return destination.experiences.find(
+    (experience) => experience.slug === props.experienceSlug
+  );
+});
+</script>
