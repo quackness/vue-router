99f607c77a093c0f7ae7254a35cea20fae20fdcf
Karolina Redden <karolina.redden@dashhudson.com>
2023-07-11 16:17:50 -0300

    add Go Back option to the web site

---
+++ src/components/GoBack.vue
@@ -0,0 +1,6 @@
+<template>
+  <span class="go-back">
+      <button @click="$router.back()">Go back</button>
+
+  </span>
+</template>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,6 +1,7 @@
 <template>
   <section v-if="destination" class="destination">
     <h1>{{ destination.name }}</h1>
+      <GoBack />
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
       <p>{{ destination.description }}</p>
@@ -31,6 +32,7 @@ import { computed, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import sourceData from "@/data.json";
 import ExperienceCard from "@/components/ExperienceCard.vue";
+import GoBack from "@/components/GoBack.vue";
 
 const route = useRoute();
 // const destination = ref(null)
